c48307c119c958bc380a01244e61154948f524da
NicholasLate <117702851+NicholasLate@users.noreply.github.com>
2023-01-12 09:52:49 +0700

    Add files via upload

---
+++ My app/main.js
@@ -0,0 +1,45 @@
+document.getElementById("calculator").addEventListener("click",function(){
+    var num1 = document.getElementById("num1").value;
+    var num2 = document.getElementById("num2").value;
+    var sum = parseInt(num1)+parseInt(num2);
+    document.getElementById("result").innerHTML = "SUM : " +sum;
+});
+document.getElementById("calculator2").addEventListener("click",function(){
+    var num1 = document.getElementById("num1").value;
+    var num2 = document.getElementById("num2").value;
+    var sum = num1-num2;
+    document.getElementById("result2").innerHTML = "SUM : " +sum;
+});
+document.getElementById("calculator3").addEventListener("click",function(){
+    var num1 = document.getElementById("num1").value;
+    var num2 = document.getElementById("num2").value;
+    var sum = num1*num2;
+    document.getElementById("result3").innerHTML = "SUM : " +sum;
+});
+document.getElementById("calculator4").addEventListener("click",function(){
+    var num1 = document.getElementById("num1").value;
+    var num2 = document.getElementById("num2").value;
+    var sum = num1/num2;
+    document.getElementById("result4").innerHTML = "SUM : " +sum;
+});
+function myalert(){
+    var num1 = document.getElementById("num1").value;
+    var num2 = document.getElementById("num2").value;
+    var sum = parseInt(num1)+parseInt(num2);
+    alert(sum)
+}
+
+function bmi(){
+    var num1 = document.getElementById("num1").value;
+    var num2 = document.getElementById("num2").value;
+    var sum = num1/100;
+    var sum1 = num2 / (sum * sum)
+    alert(sum1.toFixed(2))
+}
+
+const img = document.createElement("img");
+img.src ="pic2.jpg"
+img.style.width = "400px"
+
+document.getElementById("myDiv").appendChild(img);
+
